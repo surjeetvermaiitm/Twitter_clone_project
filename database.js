a7647293629b4ec8698946a6c51d31471c4d58f2
surjeetvermaiitm <39264542+surjeetvermaiitm@users.noreply.github.com>
2021-03-30 11:31:51 +0530

    final project

---
+++ database.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+
+class Database {
+
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        mongoose.connect("mongodb://localhost:27017/twitter_clone")
+        .then(() => {
+            console.log("database connection successful");
+        })
+        .catch((err) => {
+            console.log("database connection error " + err);
+        })
+    }
+}
+
+module.exports = new Database();
